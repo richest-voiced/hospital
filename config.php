0eedd65ffa2d30d38814057d96ae685d2d7bf962
Giovanni Moretti <152964633+richest-voiced@users.noreply.github.com>
2023-12-05 20:38:35 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'your_username');
+define('DB_PASSWORD', 'your_password');
+define('DB_NAME', 'hospital_management');
+
+// Additional configuration settings can be added here
+?>
