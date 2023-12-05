3f1cae18ea9464292c6b134807ae56d9b9deb82e
Giovanni Moretti <152964633+richest-voiced@users.noreply.github.com>
2023-12-05 20:38:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// index.php
+
+require_once 'config.php';
+require_once 'functions.php';
+require_once 'db.php';
+require_once 'patient.php';
+require_once 'doctor.php';
+require_once 'appointment.php';
+
+// Your application logic goes here
+?>
